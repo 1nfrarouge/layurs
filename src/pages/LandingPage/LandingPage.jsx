e6b01c6e44c6ce8119b0411cb9246812f852fdb3
1nfrarouge <samantha.yc.lim@gmail.com>
2024-03-06 01:56:35 -0500

    Add Landing Page CSS

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import './LandingPage.css'
 
 const LandingPage = () => {
   const navigate = useNavigate();
@@ -16,15 +17,18 @@ const LandingPage = () => {
     <div className="landing-page">
       <header>
         <p className="handle" onClick={goToSignup}>@LAYURS</p>
+        <img src="/images/logo.png" alt="Heart Logo" className="smaller-heart-logo"/>
+          <p className="slogan">CLOSET ORGANIZATION APP</p>
         <button className="login-button" onClick={goToLogin}>LOG IN</button>
       </header>
       <main>
         <div className="logo" onClick={goToSignup}>
-          <p className="slogan">CLOSET ORGANIZATION APP</p>
           <h1>LAYURS</h1>
+        <img src="/images/heart.png" alt="Large Heart" className="large-heart"/>
         </div>
       </main>
       <footer>
+        <img src="/images/barcode.png" alt="Barcode" className="barcode"/>
         <button className="signup-button" onClick={goToSignup}>SIGN UP</button>
       </footer>
     </div>
